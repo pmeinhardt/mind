109afc5ec10f75ca2aaf6fae3ef76fb6e4b22104
Paul Meinhardt <code@mnhrdt.io>
2024-08-31 15:46:47 +0000

    Use npm package for icons

--- app/src/Editor.tsx
+++ app/src/Editor.tsx
@@ -1,3 +1,4 @@
+import { BoltIcon } from "@heroicons/react/24/outline";
 import type { VersionVector } from "loro-crdt";
 import { Loro } from "loro-crdt";
 import { StrictMode, useEffect, useMemo, useState } from "react";
@@ -124,20 +125,7 @@ function Application({ doc }: Props) {
                     href="#"
                   >
                     <span className="">Collaborate</span>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="-mx-1 size-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
-                      />
-                    </svg>
+                    <BoltIcon className="-mx-1 size-6" aria-hidden />
                   </a>
                 </li>
               </ul>
--- app/src/Launcher.tsx
+++ app/src/Launcher.tsx
@@ -1,3 +1,8 @@
+import {
+  ArrowDownTrayIcon,
+  DocumentArrowUpIcon,
+  SparklesIcon,
+} from "@heroicons/react/24/outline";
 import clsx from "clsx";
 import { Loro } from "loro-crdt";
 import type { ChangeEvent, DragEvent } from "react";
@@ -93,21 +98,7 @@ function Launcher({ onReady }: Props) {
             type="button"
             onClick={createNewDoc}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="-ml-1 size-5"
-              aria-hidden
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z"
-              />
-            </svg>
+            <SparklesIcon className="-ml-0.5 size-5" aria-hidden />
             Start from scratch
           </button>
           <span className="font-light text-zinc-800">or</span>
@@ -115,20 +106,7 @@ function Launcher({ onReady }: Props) {
             className="flex cursor-pointer items-center gap-1 text-nowrap rounded-lg bg-blue-300/30 px-3 py-2 font-normal text-blue-600 transition-colors hover:bg-blue-500/90 hover:text-white"
             htmlFor="file-input"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-5"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m6.75 12-3-3m0 0-3 3m3-3v6m-1.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
-              />
-            </svg>
+            <DocumentArrowUpIcon className="-ml-0.5 size-5" aria-hidden />
             Upload an existing state
           </label>
           <input
@@ -158,20 +136,7 @@ function Launcher({ onReady }: Props) {
           !isDragging && "hidden",
         )}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="size-8"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
-          />
-        </svg>
+        <ArrowDownTrayIcon className="size-8" aria-hidden />
         <p className="text-4xl font-semibold">Let go to upload</p>
       </div>
     </div>
