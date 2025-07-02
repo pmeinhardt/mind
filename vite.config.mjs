86654d3d2d6ebda4e380b51a4a268571323d2d92
pmeinhardt <code@mnhrdt.io>
2025-07-02 23:59:26 +0200

    Sort imports in vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,7 @@
 /// <reference types="vitest" />
 
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tla from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
